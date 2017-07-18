15cb81eeeed6cacd115642b485e4062f03f0a522
João Almeida <joaoalmeida@MacBook-Pro-de-Joao.local>
2017-07-18 22:04:00 +0100

    Fixed path on makeDendograms

--- src/makeDendograms.sh
+++ src/makeDendograms.sh
@@ -3,6 +3,6 @@
 cd ../data/output/camacho-ficheiros/
 cat *_*.txt > tissueInfo.txt
 cd ../../../src/
-./count_pathways.o "/Users/joaoalmeida/Desktop/Dissertacao/sol/data/mitocondrias/feup-pp/project/data/output/camacho-ficheiros/tissueInfo.txt"
-./dendogram.R "/Users/joaoalmeida/Desktop/Dissertacao/sol/data/mitocondrias/feup-pp/project/data/output/camacho-ficheiros"
+./count_pathways.o "../data/output/camacho-ficheiros/tissueInfo.txt"
+./dendogram.R "../data/output/camacho-ficheiros"
 echo "Make Dendograms Finished, check /tree folders and graphic images generated."
